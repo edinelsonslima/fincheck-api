564025adf0ecb25fdc15074e2a8d235575db6148
edinelsonslima <edinelsonlima86@gmail.com>
2023-06-30 00:46:02 -0300

    implements bank accounts dto

--- src/modules/bank-accounts/dto/create-bank-account.dto.ts
+++ src/modules/bank-accounts/dto/create-bank-account.dto.ts
@@ -1,12 +1,21 @@
 import { BankAccount, BankAccountType } from '@prisma/client';
+import { IsHexColor, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class CreateBankAccountDto implements OmitId<BankAccount> {
-  id: string;
-
-  userId: string;
-
-  initialBalance: number;
+export class CreateBankAccountDto
+  implements OmitIdAndKeys<BankAccount, 'userId'>
+{
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsHexColor()
   color: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  initialBalance: number;
+
   type: BankAccountType;
 }
