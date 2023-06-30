db3509e3c44305464e0546c7fcd61ac0376d208e
edinelsonslima <edinelsonlima86@gmail.com>
2023-06-30 00:45:34 -0300

    improved omit types

--- src/shared/@types/omit-id.d.ts
+++ src/shared/@types/omit-id.d.ts
@@ -1,1 +1,6 @@
-type OmitId<Type> = Omit<Type, 'id'>;
+type OmitId<Type extends object> = Omit<Type, 'id'>;
+
+type OmitIdAndKeys<Type extends object, Keys extends keyof Type> = Omit<
+  Type,
+  'id' | Keys
+>;
