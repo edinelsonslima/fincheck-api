6ba343e85d3933a913e77eab872a25f32374319a
edinelsonslima <edinelsonlima86@gmail.com>
2023-07-03 23:26:09 -0300

    implements crud transactions

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,41 @@
-export class CreateTransactionDto {}
+import { Transaction, TransactionType } from '@prisma/client';
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateTransactionDto
+  implements OmitIdAndKeys<Transaction, 'userId' | 'date'>
+{
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  bankAccountId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  categoryId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  value: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: string;
+
+  @IsNotEmpty()
+  @IsEnum(TransactionType)
+  type: TransactionType;
+}
--- src/modules/transactions/transactions.controller.ts
+++ src/modules/transactions/transactions.controller.ts
@@ -3,38 +3,54 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
+  ParseUUIDPipe,
   Post,
   Put,
 } from '@nestjs/common';
+import { ActiveUserId } from 'src/shared/decorators/active-user-id';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
-import { TransactionsService } from './transactions.service';
+import { TransactionsService } from './services/transactions.service';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionsService.create(createTransactionDto);
+  async create(
+    @ActiveUserId() userId: string,
+    @Body() createTransactionDto: CreateTransactionDto,
+  ) {
+    return this.transactionsService.create(userId, createTransactionDto);
   }
 
   @Get()
-  findAll() {
-    return this.transactionsService.findAll();
+  findAllByUserId(@ActiveUserId() userId: string) {
+    return this.transactionsService.findAllByUserId(userId);
   }
 
-  @Put(':id')
+  @Put(':transactionId')
   update(
-    @Param('id') id: string,
+    @ActiveUserId() userId: string,
     @Body() updateTransactionDto: UpdateTransactionDto,
+    @Param('transactionId', ParseUUIDPipe) transactionId: string,
   ) {
-    return this.transactionsService.update(+id, updateTransactionDto);
+    return this.transactionsService.update(
+      userId,
+      transactionId,
+      updateTransactionDto,
+    );
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionsService.remove(+id);
+  @Delete(':transactionId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(
+    @ActiveUserId() userId: string,
+    @Param('transactionId', ParseUUIDPipe) transactionId: string,
+  ) {
+    return this.transactionsService.remove(userId, transactionId);
   }
 }
